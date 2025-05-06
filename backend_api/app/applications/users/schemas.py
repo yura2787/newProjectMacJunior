1e6fbaa99c46dc07a5e2779d284072b363dd5f76
urijtrufin <yura@gmail.com>
2025-05-06 21:02:24 +0300

    fix codestyl

--- backend_api/app/applications/users/schemas.py
+++ backend_api/app/applications/users/schemas.py
@@ -2,9 +2,7 @@ from pydantic import BaseModel, EmailStr, Field, ValidationInfo, model_validator
 
 
 class BaseFields(BaseModel):
-    email: EmailStr = Field(
-        description="User email", examples=["test_hillel_api_mailing@ukr.net"]
-    )
+    email: EmailStr = Field(description="User email", examples=["test_hillel_api_mailing@ukr.net"])
     name: str = Field(description="User nickname", examples=["Casper"])
 
 
