c560159b7c5df817016120b85fc0098e7d63d8f5
urijtrufin <yura@gmail.com>
2025-05-08 18:05:21 +0300

    add

--- backend_api/app/applications/users/router.py
+++ backend_api/app/applications/users/router.py
@@ -8,4 +8,3 @@ router_users = APIRouter()
 @router_users.post("/create", status_code=status.HTTP_201_CREATED)
 async def create_user(new_user: RegisterUserFields) -> BaseFields:
     return new_user
-
