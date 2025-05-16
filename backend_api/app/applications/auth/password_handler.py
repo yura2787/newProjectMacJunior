446c49880d5069995697bc4f17a897dff2d5953f
urijtrufin <yura@gmail.com>
2025-05-16 19:13:32 +0300

    refactor

--- backend_api/app/applications/auth/password_handler.py
+++ backend_api/app/applications/auth/password_handler.py
@@ -1,15 +1,13 @@
-from passlib.context import CryptoContext
-from typing_extensions import deprecated
+from passlib.context import CryptContext
 
 
 class PasswordEncrypt:
 
-    pwd_context = CryptoContext(schemas=['bcrypt'], deprecated='auto')
+    pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
     @classmethod
     async def get_password_hash(cls, password: str) -> str:
-            return cls.pwd_context.hash(password)
-
+        return cls.pwd_context.hash(password)
 
     @classmethod
     async def verify_password(cls, plain_password: str, hashed_password: str) -> bool:
