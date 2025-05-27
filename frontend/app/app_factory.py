d442bdd4e93edbb333dbe29190903390c24775e9
urijtrufin <yura@gmail.com>
2025-05-27 19:31:00 +0300

    new

--- frontend/app/app_factory.py
+++ frontend/app/app_factory.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
 
+from routers.main_page_router import router
 from settings import settings
 
 
 def get_application() -> FastAPI:
     app = FastAPI(debug=settings.DEBUG)
+    app.include_router(router)
 
     return app
