f6847155cc90bfbf678b912262796600fbd1b83d
urijtrufin <yura@gmail.com>
2025-05-30 19:06:02 +0300

    setup login

--- frontend/app/routers/main_page_router.py
+++ frontend/app/routers/main_page_router.py
@@ -1,20 +1,49 @@
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, Request, Form
 from fastapi.templating import Jinja2Templates
-
+import httpx
 router = APIRouter()
+templates = Jinja2Templates(directory='templates'
 
-templates = Jinja2Templates(directory='templates')
-
-
+                            )
 @router.get('/')
 async def index(request: Request):
-    context = {'request': request, 'data': 123}
+    context = {'request': request}
     response = templates.TemplateResponse('index.html', context=context)
     return response
+async def login_user(user_email: str, password: str):
+    async with httpx.AsyncClient() as client:
+        response = await client.post(
+            url='http://backend_api:9999/api/auth/login',
+            data={"username": user_email, 'password': password}
 
+        )
+        print(response.json())
+        return response.json()
+
+
+async def get_user_info(access_token: str):
+    async with httpx.AsyncClient() as client:
+        response = await client.get(
+            url='http://backend_api:9999/api/auth/get-my-info',
+            headers={"Authorization": f'Bearer {access_token}'}
+
+        )
+        print(response.json())
+        return response.json()
 
 @router.get('/login')
-async def login(request: Request):
-    context = {'request': request, 'data': 123}
-    response = templates.TemplateResponse('index.html', context=context)
+@router.post('/login')
+async def login(request: Request, user_email: str = Form(''), password: str = Form('')):
+    print(F"{user_email}")
+    print(F"{password}")
+
+    user_tokens = await login_user(user_email, password)
+    access_token = user_tokens.get('access_token')
+    user = None
+    if access_token:
+        user = await get_user_info(access_token)
+
+
+    context = {'request': request, 'user': user}
+    response = templates.TemplateResponse('login.html', context=context)
     return response
