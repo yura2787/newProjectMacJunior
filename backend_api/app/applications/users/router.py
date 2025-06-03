b67eb8d205e8d6783b717c0b3b5caadf0285ada1
urijtrufin <yura@gmail.com>
2025-06-03 20:03:25 +0300

    create user in web

--- backend_api/app/applications/users/router.py
+++ backend_api/app/applications/users/router.py
@@ -1,7 +1,8 @@
-from fastapi import APIRouter, Depends, status
+
+from fastapi import APIRouter, Depends, status, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from applications.users.crud import create_user_in_db
+from applications.users.crud import create_user_in_db, get_user_by_email
 from applications.users.schemas import BaseFields, RegisterUserFields
 from database.session_dependenscise import get_async_session
 
@@ -13,5 +14,10 @@ router_users = APIRouter()
 
 @router_users.post("/create", status_code=status.HTTP_201_CREATED)
 async def create_user(new_user: RegisterUserFields, session: AsyncSession = Depends(get_async_session)) -> BaseFields:
+    user = await get_user_by_email(new_user.email, session)
+    user = await get_user_by_email(new_user.email, session)
+    if user:
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail='Already exists')
+
     await create_user_in_db(new_user.email, new_user.name, new_user.password, session)
     return new_user
