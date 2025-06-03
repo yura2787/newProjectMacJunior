a04a6decc123946fb78546385412a77a2e84c2fd
urijtrufin <yura@gmail.com>
2025-06-03 19:47:50 +0300

    add sentry

--- backend_api/app/app_factory.py
+++ backend_api/app/app_factory.py
@@ -3,7 +3,12 @@ from fastapi import FastAPI
 from applications.auth.router import router_auth
 from applications.users.router import router_users
 from settings import settings
+import sentry_sdk
 
+sentry_sdk.init(
+    dsn=settings.SENTRY,
+    send_default_pii=True,
+)
 
 def get_application() -> FastAPI:
     app = FastAPI(root_path="/api", root_path_in_servers=True, debug=settings.DEBUG)
