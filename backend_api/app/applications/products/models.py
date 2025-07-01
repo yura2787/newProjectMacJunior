df6f5775e999ed6401797658b3234c7020be229d
urijtrufin <yura@gmail.com>
2025-07-01 19:52:07 +0300

    cart 1

--- backend_api/app/applications/products/models.py
+++ backend_api/app/applications/products/models.py
@@ -2,7 +2,7 @@ import uuid
 from datetime import datetime
 from sqlalchemy.dialects.postgresql import ARRAY
 
-from sqlalchemy import String
+from sqlalchemy import String, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy.sql import func
 
@@ -10,11 +10,15 @@ from database.base_model import Base
 
 
 
-class Product(Base):
-    __tablename__ = "products"
 
+class ModelCommonMixin:
     id: Mapped[int] = mapped_column(primary_key=True)
     created_at: Mapped[datetime] = mapped_column(default=func.now())
+
+
+class Product(ModelCommonMixin, Base):
+    __tablename__ = "products"
+
     uuid_data: Mapped[uuid.UUID] = mapped_column(default=uuid.uuid4)
 
     title: Mapped[str] = mapped_column(String(100), index=True, nullable=False)
@@ -25,3 +29,23 @@ class Product(Base):
 
     def __str__(self):
         return f'Product {self.title} - {self.id}'
+
+
+class Cart(ModelCommonMixin, Base):
+    __tablename__ = "carts"
+
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+    is_closed: Mapped[bool] = mapped_column(default=False)
+
+
+class CartProduct(ModelCommonMixin, Base):
+    __tablename__ = "cart_products"
+
+    cart_id: Mapped[int] = mapped_column(ForeignKey("carts.id"))
+    product_id: Mapped[int] = mapped_column(ForeignKey("products.id"))
+    price: Mapped[float] = mapped_column(default=0.0)
+    quantity: Mapped[float] = mapped_column(default=0.0)
+
+    @property
+    def total(self) -> float:
+        return self.price * self.quantity
